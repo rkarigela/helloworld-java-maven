9f008933084b14c752baf6ecfe9a7e902b0dd7ce
rkarigela <rk.arigela@gmail.com>
2018-12-24 21:32:10 +0530

    ARK-2

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World RKAR-2" );
+        System.out.println( "Hello World ARK-2" );
     }
 }
